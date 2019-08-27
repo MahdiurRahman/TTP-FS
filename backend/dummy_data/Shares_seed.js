cdfa2d3b6e71554a518a9ca7238d6e2a9a493d03
Mahdiur Rahman <rmahdiur@gmail.com>
2019-08-27 19:56:07 -0400

    All user GET routes configured. Also Shares have been seeded to db

---
+++ backend/dummy_data/Shares_seed.js
@@ -0,0 +1,16 @@
+const Shares_seed = [
+    {
+        symbol: 'SNAP',
+        quantity: 10
+    },
+    {
+        symbol: 'AAPL',
+        quantity: 5
+    },
+    {
+        symbol: 'FB',
+        quantity: 20
+    }
+]
+
+module.exports = Shares_seed
