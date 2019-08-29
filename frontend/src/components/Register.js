1374433c9edeac3195143257a073f4445c6e7635
Mahdiur Rahman <rmahdiur@gmail.com>
2019-08-28 23:10:25 -0400

    minor changes

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 
 class Register extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            passwordCheck: ''
+        }
+    }
+
+    onChangeHandler = event => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log(this.state)
+    }
+
+    onSubmitHandler = event => {
+        event.preventDefault()
+    }
+
     render() {
         return (
             <div>
-                <form onSubmit={() => console.log("REGISTER: onSubmit")}>
-                    <input name="firstName" type="text" placeholder="first name" />
-                    <input name="lastName" type="text" placeholder="last name" />
-                    <input name="email" type="email" placeholder="email" />
-                    <input name="password" type="password" placeholder="password" />
-                    <input name="passwordCheck" type="password" placeholder="retype password" />
+                <form onSubmit={this.onSubmitHandler}>
+                    <input name="firstName" type="text" placeholder="first name" onChange={this.onChangeHandler} />
+                    <input name="lastName" type="text" placeholder="last name" onChange={this.onChangeHandler} />
+                    <input name="email" type="email" placeholder="email" onChange={this.onChangeHandler} />
+                    <input name="password" type="password" placeholder="password" onChange={this.onChangeHandler} />
+                    <input name="passwordCheck" type="password" placeholder="retype password" onChange={this.onChangeHandler} />
                     <button type="submit">Register</button>
                 </form>
                 <Link to="/">Login</Link>
